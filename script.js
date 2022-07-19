a20dad751e8e787ab77b440dac6cbcd15b94013d
berlin <0xb3rlin@gmail.com>
2022-07-18 23:53:30 -0500

    added RPS UI: buttons, score, historical game

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 var gameChoices = ["rock", "paper", "scissors"];
 let computerScore = 0;
 let playerScore = 0;
+let gamesTied = 0;
+let roundsPlayed = 0;
+let playerSelection = null;
+let winner = null;
+let roundsToPlay = 5;
 
 // getComputerChoice function randomly generates rock paper or scissor
 function getComputerChoice() {
@@ -12,14 +17,11 @@ function getComputerChoice() {
 
 // playerSelection prompt for player's selection, make it case insensitive
 function getPlayerChoice() {
-  let playerSelection = prompt("What do you pick?").toLowerCase();
   while (true) {
     if (gameChoices.includes(playerSelection)) {
-      //   console.log("good");
       return playerSelection;
       break;
     } else {
-      //   console.log("incorrect option");
       playerSelection = prompt(
         `Invalid\n Pick only: ${gameChoices[0]}, ${gameChoices[1]}, or ${gameChoices[2]}`
       ).toLowerCase();
@@ -31,49 +33,56 @@ function getPlayerChoice() {
 function playRound(playerSelection, computerSelection) {
   console.log(`playerSelection: ${playerSelection}`);
   console.log(`computerSelection: ${computerSelection}`);
+
+  const container1 = document.querySelector("#choices-played");
+  const content1 = document.createElement("div");
+  content1.textContent = `Player: ${playerSelection} vs. Computer: ${computerSelection}`;
+  container1.appendChild(content1);
+  roundsPlayed++;
+
   switch (true) {
     case playerSelection === computerSelection:
       console.log("Tie!");
+      gamesTied++;
       return 0;
       break;
     case playerSelection == "rock" && computerSelection == "scissors":
     case playerSelection == "scissors" && computerSelection == "paper":
     case playerSelection == "paper" && computerSelection == "rock":
       console.log(`You Won! ${playerSelection} beats ${computerSelection}`);
+      playerScore++;
       return 1;
       break;
     default:
       console.log(`You Lost... ${computerSelection} beats ${playerSelection}`);
+      computerScore++;
       return -1;
       break;
   }
 }
 
-// Simulates a game of x rounds
-function playGame(rounds) {
-  for (let i = 0; i < rounds; i++) {
-    let score = playRound(getPlayerChoice(), getComputerChoice());
-    switch (true) {
-      case score == 1:
-        playerScore++;
-        break;
-      case score == -1:
-        computerScore++;
-        break;
-    }
-    console.log(`Computer: ${computerScore} \nPlayer: ${playerScore}`);
-  }
-  console.log(
-    `The final score\nComputer: ${computerScore} \nPlayer: ${playerScore}`
-  );
-  if (playerScore == computerScore) {
-    console.log(`You tied best out of ${rounds} rounds`);
-  } else if (playerScore > computerScore) {
-    console.log(`You won best out of ${rounds} rounds`);
-  } else {
-    console.log(`You lost best out of ${rounds} rounds`);
-  }
-}
-
 // Run function game with x rounds
-playGame(3);
+window.addEventListener("click", function (e) {
+  let playerSelection = e.target.dataset.choice;
+  if (gameChoices.includes(playerSelection)) {
+    playRound(playerSelection, getComputerChoice());
+  }
+
+  const playerScore1 = document.querySelector("#player-score");
+  const computer1 = document.querySelector("#computer-score");
+  const roundsPlayed1 = document.querySelector("#rounds-played");
+  const gamesTied1 = document.querySelector("#tied-score");
+  playerScore1.textContent = `${playerScore}`;
+  computer1.textContent = `${computerScore}`;
+  roundsPlayed1.textContent = `${roundsPlayed}`;
+  gamesTied1.textContent = `${gamesTied}`;
+
+  if (playerScore == roundsToPlay || computerScore == roundsToPlay) {
+    if (
+      playerScore > computerScore ? (winner = "Player") : (winner = "Computer")
+    )
+      alert(
+        `${winner} is the winner!\nFinal Score:\nPlayer: ${playerScore}\nComputer: ${computerScore}`
+      );
+  }
+});
